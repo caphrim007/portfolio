c47cd9f79c8a5aa4942efce8c5f9551c6c3cfe25
Tim Rupp <caphrim007@gmail.com>
2013-01-29 16:02:10 -0600

    Made js code just loop over stuff

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,28 +1,22 @@
 $(document).ready(function() {
-	$(".savory-thumb, .deepsee-thumb, .greasemonkey-thumb, .resolver-thumb, .nessquik-thumb, .fsf-thumb").fancybox({
-		prevEffect: 'none',
-		nextEffect: 'none',
-		helpers	: {
-			title: { type: 'inside' }
-		}
-	});
+	var items;
 
-	$('.savory-trig').click(function(){
-		$('.savory-thumb').first().trigger('click');
-	});
-	$('.deepsee-trig').click(function(){
-		$('.deepsee-thumb').first().trigger('click');
-	});
-	$('.greasemonkey-trig').click(function(){
-		$('.greasemonkey-thumb').first().trigger('click');
-	});
-	$('.resolver-trig').click(function(){
-		$('.resolver-thumb').first().trigger('click');
-	});
-	$('.nessquik-trig').click(function(){
-		$('.nessquik-thumb').first().trigger('click');
-	});
-	$('.fsf-trig').click(function(){
-		$('.fsf-thumb').first().trigger('click');
+	items = [
+		'savory', 'deepsee', 'greasemonkey', 'resolver', 'nessquik', 'fsf',
+		'portscanmenow'
+	];
+
+	$.each(items, function(idx, value) {
+		$("." + value + "-thumb").fancybox({
+			prevEffect: 'none',
+			nextEffect: 'none',
+			helpers	: {
+				title: { type: 'inside' }
+			}
+		});
+
+		$('.' + value + '-trig').click(function(){
+			$('.' + value + '-thumb').first().trigger('click');
+		});
 	});
 });
