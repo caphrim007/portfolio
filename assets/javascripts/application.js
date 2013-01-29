f831e4d93a76eb28c69a98275f00fb26d8f1f29f
Tim Rupp <caphrim007@gmail.com>
2013-01-29 09:44:40 -0600

    Import of fsf software details

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$(".savory-thumb, .deepsee-thumb, .greasemonkey-thumb, .resolver-thumb, .nessquik-thumb").fancybox({
+	$(".savory-thumb, .deepsee-thumb, .greasemonkey-thumb, .resolver-thumb, .nessquik-thumb, .fsf-thumb").fancybox({
 		prevEffect: 'none',
 		nextEffect: 'none',
 		helpers	: {
@@ -21,5 +21,8 @@ $(document).ready(function() {
 	});
 	$('.nessquik-trig').click(function(){
 		$('.nessquik-thumb').first().trigger('click');
+	});
+	$('.fsf-trig').click(function(){
+		$('.fsf-thumb').first().trigger('click');
 	});
 });
