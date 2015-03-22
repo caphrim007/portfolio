2b75e0d724e1eae084d7cbfcf4d56f2aaadb6b66
Tim Rupp <caphrim007@gmail.com>
2015-03-22 12:56:47 -0500

    Updating portfolio

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,9 +2,28 @@ $(document).ready(function() {
 	var items;
 
 	items = [
-		'savory', 'deepsee', 'greasemonkey', 'resolver', 'nessquik', 'fsf',
-		'portscanmenow', 'splunk', 'puppet', 'nagios', 'git'
+		'savory',
+        'deepsee',
+        'greasemonkey',
+        'resolver',
+        'nessquik',
+        'fsf',
+		'portscanmenow',
+        'splunk',
 	];
+
+    $('.img-trigger').on('click', function(){ 
+        var e_id = $(this).attr('id');
+		$("#" + e_id + "-thumb").fancybox({
+			prevEffect: 'none',
+			nextEffect: 'none',
+			helpers	: {
+				title: { type: 'inside' }
+			}
+		});
+
+		$('#' + e_id + '-thumb').first().trigger('click');
+    });
 
 	$.each(items, function(idx, value) {
 		$("." + value + "-thumb").fancybox({
